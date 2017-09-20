352e5e0c692bfe5153cfae4cff10f69da0a94b4e
elwpdlqk <elwpdlqk@gmail.com>
2017-09-20 22:35:48 +0900

    로또 추첨기 변형

--- lotto/js/lotto.js
+++ lotto/js/lotto.js
@@ -1,37 +1,71 @@
 
 //번호를 담을 배열을 생성한다.
-var array = [];
-
-//45개의 번호를 array 배열에 넣는다.
-    for (var i = 1; i < 46; i++) {
-        array.push(i);
-    }
-
 //출력할 6개의 숫자를 담을 배열을 만든다.
-    var myNum = [];
+
 
 //랜덤하게 6개의 숫자를 뽑는다.
-    for (var j = 1; j <= 100; j++) { //j의 범위는 6을 넘어가게.
-        var randomNum = Math.floor(Math.random() * array.length) + 1
-        
+var createRandomNumbers = function (minNum, maxNum,count) {
+    //최대값이 최소값보다 작거나 같을경우
+    //출력하고자 하는 개수가 0이거나 음수일 경우
+    //입력되는 값이 숫자가 아닌 경우
+    //매개변수를 더 많거나 적게 쓸 경우
+    if (arguments.length !== 3) {
+        console.error('매개변수 갯수가 3개가 아님.')
+        return
+    }
+    for (key in arguments) {
+        if (typeof arguments[key] !== 'number') {
+            console.error('매개변수중 숫자가 아닌것이 있습니다.')
+            return
+        } 
+    }
+    
+    var myNum = [];
+    while (myNum.length < count) { //j의 범위는 6을 넘어가게.
+        var RandomNumber = Math.floor(Math.random() * (maxNum - minNum) + minNum) + 1
         //중복되는 경우를 제거한다.
-        if (myNum.indexOf(randomNum) === -1) {
-            myNum.push(randomNum);
-        }
-        
+        if (myNum.indexOf(RandomNumber) === -1) {
+            myNum.push(RandomNumber);        
+        } 
         //중복되는 경우에 6개의 숫자가 출력되지 않는 경우가 있으므로
         //범위를 크게 지정 6개가 산출되면 동작을 멈추게 한다.
-        
-        if (myNum.length === 6) {
-            break
-        }
     }
+    
+    printNumbers(myNum)
+}
+
+var printNumbers = function (numbers) {
+    // #result 에 li 태그로 결과 추가
+}
 //출력된 숫자를 오름차순으로 정렬한다.
-    myNum.sort(function (a,b) {
-        return a - b;
-    });
+/*myNum.sort(function (a,b) {
+    return a - b;
+});*/
 
 //버튼 클릭시 동작시킬 함수.
-function outText() {
-    alert(myNum);
+
+
+
+
+//  1 - 46 까지의 숫자를 담고 있는 배열
+// 배열 1개, for문, 랜덤
+// shuffle
+//   - swap
+
+// shuffle : min - max 까지 갯수의 배열을 만들고 순서를 섞는다
+var shuffle = function (min, max) {
+//    var data = [0, 1]
+//    var num = data[0]    
+//    data[0] = data[1]
+//    data[1] = num
+    var arr = []
+    for (var i = 0; i < max - min + 1; i++) {
+        arr.push(min + i)
+    }
+    console.log(arr)
+    
+//    var shuffledArr = []
+//    return arr 또는 shuffledArr
 }
+
+var arr = shuffle(1,46)
