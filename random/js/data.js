0e3e4880f83cfe8ef01f2a890240915e86ea9d65
elwpdlqk <elwpdlqk@gmail.com>
2017-10-17 23:55:35 +0900

    데이터 스크립트 분리 생성

---
+++ random/js/data.js
@@ -0,0 +1,123 @@
+var data = {
+  results: [
+    {
+      first: 'Ara',
+      last: 'Metz',
+      email: 'Ara.Metz@amie.biz',
+      address: '5838 Cronin Road',
+      created: 'February 10, 2012',
+      balance: '$1,343.27'
+    },
+    {
+      first: 'Janis',
+      last: 'Deckow',
+      email: 'tanturtle79@gmail.com',
+      address: '1878 Considine Parkway',
+      created: 'September 15, 2014',
+      balance: '$4,665.26'
+    },
+    {
+      first: 'Shanel',
+      last: 'Collins',
+      email: 'Shanel.Collins@blaze.org',
+      address: '8115 Hauck Cliffs',
+      created: 'July 15, 2011',
+      balance: '$5,326.60'
+    },
+    {
+      first: 'Roma',
+      last: 'Howell',
+      email: 'Roma.Howell@alessia.name',
+      address: '398 Corwin Island',
+      created: 'June 21, 2010',
+      balance: '$4,097.21'
+    },
+    {
+      first: 'Cristal',
+      last: 'Mitchell',
+      email: 'orangeturtle40@gmail.com',
+      address: '624 Ebert Lodge',
+      created: 'March 6, 2009',
+      balance: '$4,565.30'
+    },
+    {
+      first: 'Marie',
+      last: 'Aufderhar',
+      email: 'Marie.Aufderhar@trever.net',
+      address: '414 Effertz Greens',
+      created: 'July 16, 2009',
+      balance: '$6,883.40'
+    },
+    {
+      first: 'Christa',
+      last: 'Heathcote',
+      email: 'Christa.Heathcote@maximus.com',
+      address: '481 Lacy Dale',
+      created: 'February 21, 2014',
+      balance: '$4,310.37'
+    },
+    {
+      first: 'Rebekah',
+      last: 'Franecki',
+      email: 'silverfrog46@gmail.com',
+      address: '865 Schamberger Camp',
+      created: 'September 23, 2010',
+      balance: '$9,394.29'
+    },
+    {
+      first: 'Caleigh',
+      last: 'Bernier',
+      email: 'pinkrabbit24@gmail.com',
+      address: '73089 Prosacco Lodge',
+      created: 'June 11, 2012',
+      balance: '$1,971.92'
+    },
+    {
+      first: 'Shaylee',
+      last: 'Ondricka',
+      email: 'limesquirrel22@gmail.com',
+      address: '71487 Joanny Prairie',
+      created: 'February 20, 2011',
+      balance: '$5,728.41'
+    },
+    {
+      first: 'Raymundo',
+      last: 'Price',
+      email: 'mintgreengiraffe91@gmail.com',
+      address: '44487 Eileen Wells',
+      created: 'August 27, 2011',
+      balance: '$2,151.35'
+    },
+    {
+      first: 'Seamus',
+      last: 'Russel',
+      email: 'Seamus.Russel@nils.name',
+      address: '003 Nikolaus Mill',
+      created: 'February 22, 2014',
+      balance: '$7,494.16'
+    },
+    {
+      first: 'Libbie',
+      last: 'Hayes',
+      email: 'silvergiraffe30@gmail.com',
+      address: '44223 Stanley Plains',
+      created: 'March 25, 2012',
+      balance: '$9,028.35'
+    }
+  ],
+  info: {
+    seed: '3f07dbd4cc558d6a',
+    results: '1',
+    page: '1',
+    version: '0.1',
+    time: {
+      instruct: 6,
+      generate: 50
+    }
+  }
+}
+data.results
+data.results.length
+data.info.page
+data.info.results
+
